59a27a255604904fedcc738aa88c7792f98f4ebe
LuffyMiner01 <78861372+LuffyMiner01@users.noreply.github.com>
2021-03-02 21:28:23 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,99 @@
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit 30 detik"
+printf "\n\n"
+sleep 95
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "akan di lanjutkan dalam 1 jam"
+sleep 3690
+start test.sh
